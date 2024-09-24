a8449a3c61c954d7dcb54c1090c109cb19a5645b
david.pereira <david.pereira@ciss.com.br>
2024-09-24 09:51:00 -0300

    Refactor and Mappers

--- src/main/java/com/david/task_manager/mapper/RoleMapper.java
+++ src/main/java/com/david/task_manager/mapper/RoleMapper.java
@@ -1,13 +1,33 @@
 package com.david.task_manager.mapper;
 
 import com.david.task_manager.domain.ENUMS.RoleEnum;
+import com.david.task_manager.domain.Role;
+import com.david.task_manager.repository.RoleRepository;
 import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public class RoleMapper {
 
-//    Set<RoleEnum>
+    protected RoleRepository roleRepository;
+
+    @Autowired
+    public void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    protected Set<Role> mapRoles(Set<RoleEnum> roleEnums) {
+        if (roleEnums == null) {
+            return null;
+        }
+        return roleEnums.stream()
+                .map(roleEnum -> roleRepository.findByName(roleEnum)
+                        .orElseThrow(() -> new RuntimeException("Role not found: " + roleEnum)))
+                .collect(Collectors.toSet());
+    }
+
 
 }
